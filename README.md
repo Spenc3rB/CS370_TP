ed36d0d470d147841b6d274c478e8c9ca023781e
spenc3rb <beersc@colostate.edu>
2024-03-30 20:04:10 -0600

    funny start to the project

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # CS370_TP
 CS370 term project
+
+## Team Members
+- Spencer Beer
+- Elaine Smith
+
+## Initialization
+
+To initialize the project, run the following command:
+```
+sudo apt upgrade && sudo apt update -y
+```
+
+To install the adafruit blinka library, run the following commands according to the [adafruit guide](https://learn.adafruit.com/circuitpython-on-raspberrypi-linux/installing-circuitpython-on-raspberry-pi):
+```
+cd ~
+pip3 install --upgrade adafruit-python-shell
+wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/raspi-blinka.py
+sudo -E env PATH=$PATH python3 raspi-blinka.py
+```
+
+Then, source the following file to set up the roomba python environment:
+```
+source ~/roomba-env/bin/activate
+```
+
+To install the required python packages, run the following command:
+```
+pip3 install -r requirements.txt
+```
+
+## Running the Project
+```bash
+python3 main.py
+```
---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, redirect, url_for
+import os
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = 'sounds'
+ALLOWED_EXTENSIONS = {'mp3'}
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = file.filename
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('index'))
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
